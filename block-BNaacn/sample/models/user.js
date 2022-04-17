7e5bc2b3c4420371b0500b6d8b7b6029750fb61f
seeratbhatia <b.seerat@gmail.com>
2022-04-17 17:54:41 +0530

    assignment-1

---
+++ block-BNaacn/sample/models/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name: String,
+    email: { type: String, lowercase: true},
+    age: { type: Number, default: 0},
+    favourities: [String],
+    marks: [Number],
+    password: { type: String, minlength: 5, maxlength: 15},
+    createAt: { type: Date, default: new Date()}
+
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
